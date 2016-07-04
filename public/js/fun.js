76d1d764e29ebf41e3a7213335726c45dc83ac7d
Милош Ђелић <milos@djelic.info>
2016-07-05 00:57:24 +0200

    Added gitignore

--- public/js/fun.js
+++ public/js/fun.js
@@ -2,53 +2,54 @@ var vals = function() {
 	this.colors = ["yellow", "red"];
 	this.cols = [0,0,0,0,0,0,0];
 	this.myTurn = false;
-	this.end = false;		
-	
+	this.end = false;
+
 	return this;
 };
 
-var values = vals();		
+var values = vals();
+var container = $("#container");
+var socket = io.connect('http://localhost:8002');
 
-var socket = io.connect('http://localhost:8000');
 socket.on("message", function(data) {
 
 	//if new move is sent from opponent
-	if (data.move) { 
+	if (data.move) {
 		values.myTurn = true;
 		$("#whoPlaysMessage").html("It's your turn. Play!");
 		console.log("received ", data.move);
-		fillTheHole(data.move, oponentColor);					
-	}	
+		fillTheHole(data.move, oponentColor);
+	}
 	//begining of the game, both players are on, server sends who plays first
 	if (data.turn) {
 		playerColor = values.colors[data.turn-1];
 		oponentColor = values.colors[data.turn % 2];
-		
-		if(data.turn == 1 && !values.end) 
+
+		if(data.turn == 1 && !values.end)
 		{
 			values.myTurn = true;
 			$("#whoPlaysMessage").html("You play first!");
 		}
 		else {
-			$("#whoPlaysMessage").html("Wait for opponent to play!");	
+			$("#whoPlaysMessage").html("Wait for opponent to play!");
 		}
-		
+
 		for( var i = 0; i < 7; i += 1) {
 			$("#arrow"+i).css("background-color", playerColor);
-		}					
-	}				
+		}
+	}
 	//server sends who won, end of the game
 	if (data.win) {
 		console.log(data.win);
 		if (data.win == "win") {$("#winner").html("<h1>YOU WON!!!</h1>");};
 		if (data.win == "lost") {$("#winner").html("<h1>YOU LOST!!!</h1>");};
 		if (data.win == "tie") {$("#winner").html("<h1>NO ONE WON.</h1>");};
-		
+
 		$("#whoPlaysMessage").css("display", "none");
-	
+
 		values.end = true;
 		values.myTurn = false;
-	}			
+	}
 });
 var sendMove = function(col){
 	values.myTurn = false;
@@ -60,46 +61,49 @@ var fillTheHole = function(col, color) {
 	$(index).css("background-color", color);
 	values.cols[col] += 1;
 };
-
-//creating board from divs
-var container = $("#container");
-for (var i = 5; i >= 0; i -= 1) {
-		var tempRow = $("<div>").attr("id", "row" + i);
-		for (var j = 0; j < 7; j += 1) {
-			var tempCell = $("<div>").attr("id", "cel" + j + i).data("col", j);
-			tempRow.append(tempCell);
-		}
-		container.append(tempRow);
+var createBoard = function(){
+	for (var i = 5; i >= 0; i -= 1) {
+			var tempRow = $("<div>").attr("id", "row" + i);
+			for (var j = 0; j < 7; j += 1) {
+				var tempCell = $("<div>").attr("id", "cel" + j + i).data("col", j);
+				tempRow.append(tempCell);
+			}
+			container.append(tempRow);
+	}
 }
 
-//listeners for selecting column 
-$("#container").on ("click", function(e) {
+$(function(){
+	createBoard();
+
+	//listeners for selecting column
+	container.on("click", function(e) {
 		if (values.myTurn && !values.end) {
-		
-		var selectedCol = $("#" + e.target.id).data("col"); 		
-		console.log(selectedCol);
-		if (cols[selectedCol] < 6) { 	// limit check, if whole column if full do nothing
-		
-			fillTheHole(selectedCol, playerColor);
-			sendMove(selectedCol);
-			console.log("after sending ", selectedCol);
-			$("#arrow"+selectedCol).css("visibility","hidden");	
-			setTimeout(function(){
-				$("#arrow"+selectedCol).css("visibility","visible");	
-			}, 300);
-		}	
-	}
-});
-// on hover over the columns coin on top (arrow divs) appear
-$("#container").children().hover(
-	function(e) {
-		$("#arrows > span").css("visibility","hidden");
-		var selectedCol = $("#" + e.target.id).data("col"); 
-		$("#arrow"+selectedCol).css("visibility","visible");
-		
-	},
-	function(e){
-		var selectedCol = e.target.id.charAt(3); 
-		$("#arrow"+selectedCol).css("visibility","hidden");
-	}
-);
+			var selectedCol = $("#" + e.target.id).data("col");
+			console.log(selectedCol);
+			if (cols[selectedCol] < 6) { 	// limit check, if whole column if full do nothing
+				fillTheHole(selectedCol, playerColor);
+				sendMove(selectedCol);
+				console.log("after sending ", selectedCol);
+				$("#arrow"+selectedCol).css("visibility","hidden");
+				setTimeout(function(){
+					$("#arrow"+selectedCol).css("visibility","visible");
+				}, 300);
+			}
+		}
+	});
+
+	// on hover over the columns coin (arrow divs) appears on top
+	container.children().hover(
+		function(e) {
+			$("#arrows > span").css("visibility","hidden");
+			var selectedCol = $("#" + e.target.id).data("col");
+			$("#arrow"+selectedCol).css("visibility","visible");
+
+		},
+		function(e){
+			var selectedCol = e.target.id.charAt(3);
+			$("#arrow"+selectedCol).css("visibility","hidden");
+		}
+	);
+
+})
